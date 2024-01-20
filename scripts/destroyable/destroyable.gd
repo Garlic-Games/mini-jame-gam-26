7e221dca5badf77f58c5ca157cd60943ca588426
Nanocirs <nanocirs@gmail.com>
2024-01-20 16:05:27 +0100

    Added destroyable buildings (test with K)

--- scripts/destroyable/destroyable.gd
+++ scripts/destroyable/destroyable.gd
@@ -3,15 +3,35 @@ extends Node
 @export var minimumSpeed : float = 20.0;
 @export var explosionForce : float = 15.0;
 
-var pieces : Array;
 var explosionCenter : Vector3 = Vector3.ZERO;
 
+var piecesNode : Node = null;
+var modelNode : Node = null;
+
+var pieces : Array = [];
+
 func _ready():
-	FindRigidBodies(get_tree().get_root(), pieces);
+	FindRigidBodies(self, pieces);
+
+	if piecesNode:
+		piecesNode.set_process(false);
+		piecesNode.hide();
 	
+	set_process_input(true) 
+	
+func _input(ev):
+	if Input.is_key_pressed(KEY_K):
+		Explode();
+		
 func Explode():
 	var forceDirection : Vector3;
 	var randomVector : Vector3;
+	
+	piecesNode.set_process(true);
+	piecesNode.show();
+	
+	modelNode.set_process(false);
+	modelNode.hide();
 	
 	for piece in pieces:
 		forceDirection = explosionCenter.direction_to(piece.position);
@@ -24,6 +44,12 @@ func FindRigidBodies(node : Node, pieceContainer : Array):
 		if child is RigidBody3D:
 			pieceContainer.append(child);
 			child.gravity_scale=0;
+			
+			if piecesNode == null:
+				piecesNode = node;
 
 		if child.get_child_count() > 0:
 			FindRigidBodies(child, pieces);
+		
+		elif modelNode == null:
+			modelNode = node;
