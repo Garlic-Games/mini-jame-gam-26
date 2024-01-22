22c99963c84e48c55c7f8edce1c72e23d6234fca
Nanocirs <nanocirs@gmail.com>
2024-01-22 13:54:41 +0100

    FPS counter can now be toggled with F9

--- scripts/ui/FPSCounter.gd
+++ scripts/ui/FPSCounter.gd
@@ -1,4 +1,16 @@
 extends Label
 
-func _process(delta: float):
-	text = str("%.2f" % (1.0/delta), " FPS\n", "%.2f" % (1000.0 * delta), " ms");
+var isEnabled : bool = false;
+
+func _process(delta: float):	
+	if isEnabled:
+		text = str("%.2f" % (1.0/delta), " FPS\n", "%.2f" % (1000.0 * delta), " ms");
+		
+	if Input.is_action_just_pressed("show_fps"):
+		isEnabled = !isEnabled;
+		
+		if isEnabled: 
+			get_parent().show();
+			
+		else: 
+			get_parent().hide();
