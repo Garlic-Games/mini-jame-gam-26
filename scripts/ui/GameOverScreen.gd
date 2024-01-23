16274075493b35a6aec6a8f2d9b92f158d093f00
Nanocirs <nanocirs@gmail.com>
2024-01-23 12:33:48 +0100

    Minor changes

--- scripts/ui/GameOverScreen.gd
+++ scripts/ui/GameOverScreen.gd
@@ -12,18 +12,18 @@ func _ready():
 
 
 func _on_game_over():	
+	call_deferred("pause_tree");
+	
 	nftLabel.text = str(game_manager.current_treasures, "/", game_manager.max_treasures);
 	
 	if game_manager.state == game_manager.GAME_STATES.WIN:
 		winLabel.show();
-		pass;
 	
 	elif game_manager.state == game_manager.GAME_STATES.GAMEOVER:
 		winLabel.hide();
 	
 	show();
 	
-	call_deferred("pause_tree");
 
 func pause_tree():
 	get_tree().paused = true;
