9e901848c249f84d5f7f79bc14d72e16dac94343
Nanocirs <nanocirs@gmail.com>
2024-01-23 13:33:56 +0100

    Minor changes

--- scripts/ui/glitch_on_points.gd
+++ scripts/ui/glitch_on_points.gd
@@ -1,15 +1,16 @@
 extends TextureRect
 
+@export var duration: float = 1;
 
 var game_manager: GameManager;
-
 var last: float = 0;
-@export var duration: float = 1;
 var isActive: bool = false;
+
 
 func _ready():
 	game_manager = get_node("/root/Gamemanager");
 	game_manager.connect("points_added", _on_points_added);
+
 
 func _process(delta):
 	if isActive:
@@ -19,10 +20,7 @@ func _process(delta):
 			material.set_shader_parameter("doeet", false);
 			
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_points_added():
 	material.set_shader_parameter("doeet", true);
 	last = 0.0;
 	isActive = true;
-
-	
